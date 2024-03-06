19f17b98a2c2f7454f6def3cb5164cd585e09a60
huy89201 <levanhuy08092001@gmail.com>
2024-03-06 22:20:02 +0700

    loading state and error toast

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -8,25 +8,55 @@ import MovieItemListView from "../movieItem/listView/MovieItemListView";
 import { initMovieSate } from "../../store/slice/moiveSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hook";
 import { useEffect, useMemo } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+interface CustomToastProps {
+  message: string;
+  type: "success" | "error" | "info" | "warning";
+}
 
 export default function MovieList() {
   const dispatch = useAppDispatch();
 
-  const { page, isListView, movieListType, query, totalpage } = useAppSelector(
+  const { page, isListView, movieListType, query } = useAppSelector(
     (state) => state.movies
   );
 
-  const { data: movieByMovieType, isLoading } = useGetMovieNowPlayinngQuery({
+  const {
+    data: movieByMovieType,
+    isLoading: movieByMovieTypeLoading,
+    error: movieByMovieTypeError,
+  } = useGetMovieNowPlayinngQuery({
     page,
     movieListType,
   });
 
-  const { data: movieByQuery } = useGetMovieBySearchQuery({ page, query });
+  const {
+    data: movieByQuery,
+    error: movieByQueryError,
+    isLoading: movieByQueryLoading,
+  } = useGetMovieBySearchQuery({ page, query });
 
   const movieList = useMemo(() => {
     if (movieByQuery?.results?.length) return movieByQuery?.results;
     if (movieByMovieType?.results?.length) return movieByMovieType?.results;
   }, [movieByQuery, movieByMovieType]);
+
+  const showToast = () => {
+    toast("Something went wrong!!!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
+
+  useEffect(() => {
+    if (movieByMovieTypeError || movieByQueryError) showToast();
+  }, []);
 
   // Get init total page
   useEffect(() => {
@@ -36,17 +66,32 @@ export default function MovieList() {
       dispatch(initMovieSate(movieByMovieType.total_pages));
   }, [movieList]);
 
-  return (
-    <div className={isListView ? "" : "movie-grid-view"}>
-      {movieList
-        ? movieList.map((movie: any) => {
-            if (isListView)
-              return <MovieItemListView key={movie.id} movie={movie} />;
-            return <MovieItemGridView key={movie.id} movie={movie} />;
-          })
-        : null}
+  // console.log(movieByMovieTypeError, movieByQueryError);
 
-      {isLoading ? <div>loading...</div> : null}
-    </div>
+  return (
+    <>
+      <div className={isListView ? "" : "movie-grid-view"}>
+        {movieList
+          ? movieList.map((movie: any) => {
+              if (isListView)
+                return <MovieItemListView key={movie.id} movie={movie} />;
+              return <MovieItemGridView key={movie.id} movie={movie} />;
+            })
+          : null}
+      </div>
+      {movieByMovieTypeLoading || movieByQueryLoading ? (
+        <div className="loading-text">loading...</div>
+      ) : null}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
